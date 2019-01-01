2147345612723785d30da3bef24cb0bbdb892755
roycan <roy.canseco@gmail.com>
2019-01-01 20:43:30 +0800

    Finish working on the palindrome method

--- lib/rlcanseco_palindrome.rb
+++ lib/rlcanseco_palindrome.rb
@@ -1,6 +1,22 @@
 require "rlcanseco_palindrome/version"
 
-module RlcansecoPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+module Palindrome
+  def palindrome?
+    s = to_s.downcase.gsub(/\W+/ , "")
+    return s == s.reverse
+  end
+end
+
+class String
+
+	  include Palindrome
+
+    def letters
+      gsub(/\W+/ , "")
+    end
+end
+
+
+class Integer
+	  include Palindrome
 end
--- test/rlcanseco_palindrome_test.rb
+++ test/rlcanseco_palindrome_test.rb
@@ -1,11 +1,34 @@
 require "test_helper"
 
 class RlcansecoPalindromeTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::RlcansecoPalindrome::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
+
+  def test_non_palindrome
+    refute "apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "A man, a plan, a canal - Panama".palindrome?
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  def test_letters
+    assert_equal "MadamImAdam" , "Madam, I'm Adam.".letters 
+  end
+
 end
